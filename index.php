fc82c8261682ea567e55c1e706f9186ba21479b2
Your Name <marvin.delansorne@laplateforme.io>
2025-02-12 14:58:48 +0100

    Php index v1.01

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/style-home.css">
-    <link rel="stylesheet" href="/styles/global.css">
+    <link rel="stylesheet" href="./styles/style-home.css">
+    <link rel="stylesheet" href="./styles/global.css">
     <title>Quizz Night</title>
 </head>
 
@@ -15,7 +15,7 @@
         <nav class="navbar">
             <ul class="navbar">
                 <li>
-                    <a><img src="/img/title_navbar.png" alt="title_home"></a>
+                    <a><img src="./img/title_navbar.png" alt="title_home"></a>
                 </li>
                 <div class="navbar_p">
                     <li>
@@ -36,7 +36,7 @@
 
 
     <main>
-        <img class="women_img" src="/img/img_p_home.png" alt="img_home">
+        <img class="women_img" src="./img/img_p_home.png" alt="img_home">
         <section class="container_p1">
 
             <h1>
@@ -54,15 +54,41 @@
             </p>
         </section>
         <section class="container_p2">
-            <img class="popular_quizz" src="/img/pupular_quizz.png" alt="popular_quizz">
-            <ul>
+            <img class="popular_quizz" src="./img/pupular_quizz.png" alt="popular_quizz">
+            <ul class="children_img">
                 <li>
-                    <img src="/img/gallery_img1.png" alt="gallery_img1">
+                    <a href="#">
+                        <img src="./img/gallery_img1.png" alt="gallery_img1">
+                    </a>
                 </li>
                 <li>
-                    <img src="/img/gallery_img2.png" alt="gallery_img2">
+                    <a href="#">
+                        <img src="./img/gallery_img2.png" alt="gallery_img2">
+                    </a>
                 </li>
-            </ul>
+                <li>
+                    <a href="#">
+                        <img src="./img/gallery_img3.png" alt="gallery_img3">
+                    </a>
+                </li>
+                </ul>
+                <ul class ="children_img">
+                <li>
+                    <a href="#">
+                        <img src="./img/gallery_img4.png" alt="gallery_img4">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="./img/gallery_img5.png" alt="gallery_img5">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="./img/gallery_img6.png" alt="gallery_img6">
+                    </a>
+                </li>
+                </ul>
         </section>
     </main>
     <footer>
